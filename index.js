9f556a16810b58ed5370dfd037da039ec10965b8
Jana Zrayka <janjoune.97@hotmail.com>
2020-12-12 11:07:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+  
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  static sanitize(string){
+    return string.replace(/[^A-Za-z-0-9 '-]/g,'');
+  }
+  
+  static titleize(string){
+    let except = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let newArray = [];
+    let array = string.split(" ");
+
+    for (let i = 0; i < array.length; i++) {
+      if (i === 0) {
+        newArray.push(string.toUpperCase())
+      } else {
+        if (except.includes(array[i])) {
+          result.push(array[i])
+        } else {
+          newArray.push(string.toUpperCase())
+        }
+      }
+    }
+    return newArray.join(" ");
+  }
 }
